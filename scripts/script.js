ed3b912b8ce0cfe7dd2db59ff298a940577fde2b
Savannah <s.vanderheld@gmail.com>
2026-09-17 11:43:00 +0200

    Update JS en html
    Vooral ge experimenteerd met Javascript en HTML om het ontwerp goed te krijgen

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,11 @@
-// Grab the big image slot and all the thumbnails
 let bigImage = document.querySelector(".big img");
 let thumbs = document.querySelectorAll(".thumb");
 
-// This function will run whenever any thumbnail is clicked
 function swapImage(event) {
-  // "event" is the click. event.target is the exact thing clicked (the thumbnail image)
-  let clickedThumb = event.target;
-
-  // Read which big image this thumbnail points to
-  let targetNumber = clickedThumb.dataset.target;
-
-  // Build the filename for the matching big image
-  bigImage.src = "big" + targetNumber + ".jpg";
+    let targetFile = event.target.dataset.target;
+    bigImage.src = "assets/" + targetFile;
 }
 
-// Attach the function to every thumbnail
 thumbs.forEach(function (thumb) {
-  thumb.addEventListener("click", swapImage);
+    thumb.addEventListener("click", swapImage);
 });
